a085d0564044874d3b9e6fd1f008d2b9091718a0
dvlpr17 <d.arias17@gmail.com>
2022-03-22 12:08:54 -0600

    Integracion Captcha v2

--- pages/contacto.php
+++ pages/contacto.php
@@ -1,70 +1,112 @@
 <?php 
 
 
+$ip = $_SERVER['REMOTE_ADDR'];
+$captcha = $_POST['g-recaptcha-response'];
+$secretkey = "";
+$respuesta = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secretkey&response=$captcha&remoteip=$ip");
+
+$atributos = json_decode($respuesta, TRUE);
 
 
-$hoy = date("Y");
-if($_POST['nombre']){
 
-    $cuerpoMensaje = '
-<!DOCTYPE html>
-<html lang="es">
+if(!$atributos['success']){
+    header("Location: contacto.html");
+}else if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-<head>
-    <meta charset="utf-8">
-    <title>Amsala</title>
-</head>
-
-<body style="margin: 0; padding: 0;">
-    <table border="0" cellpadding="0" cellspacing="0" width="100%">
-        <tr>
-            <td>
-                <table align="center" border="0" cellpadding="0" cellspacing="0" width="800">
-                    <tr>
-                        <td align="center" style="padding: 25px 0px;background-color: #fff;">
-                            <img src="https://www.amsala.com.mx/imgs/varias/amsala.jpg" alt="Amsala"
-                                style="display: block;" />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td bgcolor="#f8f6f5" align="center"
-                            style="color: #84796c; margin-top:20px; font-family: Arial, sans-serif;padding:0px 0px 30px
-                            0;border-top: 1px solid #c1bcb5;border-left:#c1bcb5 1px solid;border-right:1px solid
-                            #c1bcb5;">
-                            <h1 style="font-size:30px; margin-top:20px;"><strong>Comentarios </strong></h1>
-                            <p><strong>Nombre: </strong>'.$_POST['nombre'].'</p>
-                            <p><strong>Teléfono: </strong>'.$_POST['telefono'].'</p>
-                            <p><strong>Correo: </strong>'.$_POST['correo'].'</p>
-                            <p><strong>Comentarios: </strong>'.$_POST['mensaje'].'</p>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td align="center" bgcolor="#f8f6f5"
-                            style="padding: 25px 0px; font-family: Arial, sans-serif;border-bottom: #c1bcb5 1px
-                            solid;border-left: 1px solid#c1bcb5;border-right: 1px solid #c1bcb5;color:#84796c;">
-                            <p>© '.$hoy.' Amsala</p>
-                        </td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-    </table>
-</body>
-
-</html>'; 
-
-//echo $cuerpoMensaje;
+    $nombre = $_POST['nombre'];
+    $correo = $_POST['correo'];
+    $telefono = $_POST['telefono'];
+    $mensaje = $_POST['mensaje'];
 
 
-//===========================================
-//https://www.youtube.com/watch?v=J4s9DMzUt3I&ab_channel=AbiramL%C3%B3pez
+    $hoy = date("Y");
 
-    
-    require_once ("phpmailer/clsMail.php");
-    $mailSend = new clsMail();
-    $enviado =  $mailSend->metEnviar("info@amsala.com.mx",$_POST['nombre'],$_POST['correo'],"Comentarios desde Amsala Web", $cuerpoMensaje);
+    if(empty($nombre)){
+        header("Location: contacto.html");
+    }
+    if(empty($correo)){
+        header("Location: contacto.html");
+    }
+    if(!filter_var($correo, FILTER_VALIDATE_EMAIL)){
+        header("Location: contacto.html");
+    }
+    if(empty($telefono)){
+        header("Location: contacto.html");
+    }
+    if(empty($mensaje)){
+        header("Location: contacto.html");
+    }
 
-    header("Location: gracias.html");
 
+        $cuerpoMensaje = '
+    <!DOCTYPE html>
+    <html lang="es">
+
+    <head>
+        <meta charset="utf-8">
+        <title>Amsala</title>
+    </head>
+
+    <body style="margin: 0; padding: 0;">
+        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+            <tr>
+                <td>
+                    <table align="center" border="0" cellpadding="0" cellspacing="0" width="800">
+                        <tr>
+                            <td align="center" style="padding: 25px 0px;background-color: #fff;">
+                                <img src="https://www.amsala.com.mx/imgs/varias/amsala.jpg" alt="Amsala"
+                                    style="display: block;" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td bgcolor="#f8f6f5" align="center"
+                                style="color: #84796c; margin-top:20px; font-family: Arial, sans-serif;padding:0px 0px 30px
+                                0;border-top: 1px solid #c1bcb5;border-left:#c1bcb5 1px solid;border-right:1px solid
+                                #c1bcb5;">
+                                <h1 style="font-size:30px; margin-top:20px;"><strong>Comentarios </strong></h1>
+                                <p><strong>Nombre: </strong>'.$_POST['nombre'].'</p>
+                                <p><strong>Teléfono: </strong>'.$_POST['telefono'].'</p>
+                                <p><strong>Correo: </strong>'.$_POST['correo'].'</p>
+                                <p><strong>Comentarios: </strong>'.$_POST['mensaje'].'</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="center" bgcolor="#f8f6f5"
+                                style="padding: 25px 0px; font-family: Arial, sans-serif;border-bottom: #c1bcb5 1px
+                                solid;border-left: 1px solid#c1bcb5;border-right: 1px solid #c1bcb5;color:#84796c;">
+                                <p>© '.$hoy.' Amsala</p>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+    </body>
+
+    </html>'; 
+
+    //echo $cuerpoMensaje;
+
+
+    //===========================================
+    // PHPMailer
+    //https://www.youtube.com/watch?v=J4s9DMzUt3I&ab_channel=AbiramL%C3%B3pez
+    // Captcha
+    // https://www.youtube.com/watch?v=M1jkZx2crBg&ab_channel=C%C3%B3digosdeProgramaci%C3%B3n-MR
+    //===========================================
+        
+        require_once ("phpmailer/clsMail.php");
+        $mailSend = new clsMail();
+        $enviado =  $mailSend->metEnviar("info@amsala.com.mx",$_POST['nombre'],$_POST['correo'],"Comentarios desde Amsala Web", $cuerpoMensaje);
+
+        header("Location: gracias.html");
+        
+}else{
+    header("Location: contacto.html");
 
 }
+
+
+
+
--- pages/cotizacion.php
+++ pages/cotizacion.php
@@ -38,196 +38,205 @@ $salir = "no";
 
 // print_r($_POST["v8"]);
 
-if(!empty($_POST["v8"])){
-    $a = explode(',|,',$_POST["v8"]);
-    
-    foreach($a as $element){
-        $q = [];
-        $q = explode(",",$element);
+
+//=================================================================
+// CAPTCHA V2
+
+
+$ip = $_SERVER['REMOTE_ADDR'];
+$captcha = $_POST['g-recaptcha-response'];
+$secretkey = "";
+$respuesta = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secretkey&response=$captcha&remoteip=$ip");
+
+$atributos = json_decode($respuesta, TRUE);
+
+
+
+if(!$atributos['success']){
+    header("Location: amsala.com.mx");
+}else if ($_SERVER["REQUEST_METHOD"] == "POST"){
+
+
+    if(!empty($_POST["v8"])){
+        $a = explode(',|,',$_POST["v8"]);
+        
+        foreach($a as $element){
+            $q = [];
+            $q = explode(",",$element);
+            if($q[0] == 1){
+                $w[] = $element;
+            }
+            if($q[0] == 2){
+                for($z=0;$z<count($w);$z++){
+                    $e = [];
+                    $e = explode(",",$w[$z]);
+                    if($e[2] == $q[4]){
+                        $r[] = $w[$z];
+                        $r[] = $element;
+                    }else{
+                        $r[] = $w[$z];
+                    }
+                }
+                $w=[];
+                for($s=0;$s<count($r);$s++){
+                    $w[] = $r[$s];
+                }
+                $r=[];
+            }
+        }
+    }
+
+    $q = [];
+    $parteRuta = [];
+
+
+    foreach($w as $ww){
+        
+        $q = explode(",",$ww);
         if($q[0] == 1){
-            $w[] = $element;
+            $parteRuta = explode("../",$q[1]);
+            $rutaLimpia = "https://amsala.com.mx/".$parteRuta[1];
+            $cadena2 .='<tr>';
+            $cadena2 .='<td width="100" align="center" valign="middle"><img src="'.$rutaLimpia.'" width="100"></td>';
+            $cadena2 .='<td width="100" align="center" valign="middle">'.$q[2].'</td>';
+            $cadena2 .='<td width="100" align="center" valign="middle">'.$q[3].'</td>';
+            $cadena2 .='<td width="20" align="center" valign="middle">'.$q[4].'</td>';
+            $cadena2 .='<td width="100" align="center" valign="middle"><div class="cf" style="display:inline-block;padding:10px 14px;background-color:'.$q[5].';color:'.$q[5].';">x</div></td>';
+            $cadena2 .='<tr>';
         }
         if($q[0] == 2){
-            for($z=0;$z<count($w);$z++){
-                $e = [];
-                $e = explode(",",$w[$z]);
-                if($e[2] == $q[4]){
-                    $r[] = $w[$z];
-                    $r[] = $element;
-                }else{
-                    $r[] = $w[$z];
-                }
-            }
-            $w=[];
-            for($s=0;$s<count($r);$s++){
-                $w[] = $r[$s];
-            }
-            $r=[];
+            $cadena2 .='<tr>';
+            $cadena2 .='<td width="100" align="center" valign="middle"></td>';
+            $cadena2 .='<td width="100" align="center" valign="middle"></td>';
+            $cadena2 .='<td width="100" align="center" valign="middle">'.$q[1].'</td>';
+            $cadena2 .='<td width="20" align="center" valign="middle">'.$q[2].'</td>';
+            $cadena2 .='<td width="100" align="center" valign="middle"><div class="cf" style="display:inline-block;padding:10px 14px;background-color:'.$q[3].';color:'.$q[3].';margin-bottom:5px;">x</div></td>';
+            $cadena2 .='<tr>';
         }
+        
     }
-}
-
-$q = [];
-$parteRuta = [];
 
 
-foreach($w as $ww){
-    
-    $q = explode(",",$ww);
-    if($q[0] == 1){
-        $parteRuta = explode("../",$q[1]);
-        $rutaLimpia = "https://amsala.com.mx/".$parteRuta[1];
-        $cadena2 .='<tr>';
-        $cadena2 .='<td width="100" align="center" valign="middle"><img src="'.$rutaLimpia.'" width="100"></td>';
-        $cadena2 .='<td width="100" align="center" valign="middle">'.$q[2].'</td>';
-        $cadena2 .='<td width="100" align="center" valign="middle">'.$q[3].'</td>';
-        $cadena2 .='<td width="20" align="center" valign="middle">'.$q[4].'</td>';
-        $cadena2 .='<td width="100" align="center" valign="middle"><div class="cf" style="display:inline-block;padding:10px 14px;background-color:'.$q[5].';color:'.$q[5].';">x</div></td>';
-        $cadena2 .='<tr>';
-    }
-    if($q[0] == 2){
-        $cadena2 .='<tr>';
-        $cadena2 .='<td width="100" align="center" valign="middle"></td>';
-        $cadena2 .='<td width="100" align="center" valign="middle"></td>';
-        $cadena2 .='<td width="100" align="center" valign="middle">'.$q[1].'</td>';
-        $cadena2 .='<td width="20" align="center" valign="middle">'.$q[2].'</td>';
-        $cadena2 .='<td width="100" align="center" valign="middle"><div class="cf" style="display:inline-block;padding:10px 14px;background-color:'.$q[3].';color:'.$q[3].';margin-bottom:5px;">x</div></td>';
-        $cadena2 .='<tr>';
-    }
-    
-}
-
-
-$cadena2 .='</table>';
+    $cadena2 .='</table>';
 
 
 
-$cadena = '
-<!DOCTYPE html>
-<html lang="es">
-<head>
-<meta charset="utf-8">
+    $cadena = '
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+    <meta charset="utf-8">
 
-</head>
-<body>
-    <table border="0" cellpadding="0" cellspacing="0" width="600" align="center" style="">
-        <tbody>
-            <tr>
-                <td width="210" bgcolor="" align="center" style="padding:20px 0;">
-                    <img src="http://amsala.com.mx/imgs/varias/amsala.jpg" style="display: inline-block;" width="155" height="85">
-                </td>
-            </tr>
-        </tbody>
-    </table>
-    <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="font-family:Verdana, Geneva, sans-serif; color:#282828; font-size:12px;">
-        <tbody>
-            <tr>
-                <td width="165" align="left" valign="top" style="border-bottom: 1px solid #282828;">&nbsp;</td>
-                <td width="435" align="left" valign="top" style="border-bottom: 1px solid #282828;">&nbsp;</td>
-            </tr>
-            <tr>
-                <td width="140" align="left" valign="top">&nbsp;</td>
-                <td width="460" align="left" valign="top">&nbsp;</td>
-            </tr>
-            <tr>
-                <td height="27" align="left" valign="middle"><span class="nom"><strong>Nombre:</strong></span></td>
-                <td height="27" align="left" valign="middle">'.$_POST["v1"].'</td>
-            </tr>
-            <tr>
-                <td height="30" align="left" valign="middle"><strong>E-mail:</strong></td>
-                <td align="left" valign="middle">'.$_POST["v2"].'</td>
-            </tr>
-            <tr>
-                <td height="30" align="left" valign="middle"><strong>Ciudad:</strong></td>
-                <td align="left" valign="middle">'.$_POST["v3"].'</td>
-            </tr>
-            <tr>
-                <td height="30" align="left" valign="middle"><strong>Tel&eacute;fono:</strong></td>
-                <td align="left" valign="middle">'.$_POST["v4"].'</td>
-            </tr>
-            <tr>
-                <td height="30" align="left" valign="middle"><strong>Estado:</strong></td>
-                <td align="left" valign="middle">'.$_POST["v5"].'</td>
-            </tr>
-            <tr>
-                <td height="30" align="left" valign="middle"><strong>Tipo de Cliente:</strong></td>
-                <td align="left" valign="middle">'.$_POST["v6"].'</td>
-            </tr>
-            <tr>
-                <td height="27" align="left" valign="middle"><span class="nom"><strong>Preguntas y/o Comentarios:</strong></span></td>
-                <td height="27" align="left" valign="middle">'.$_POST["v7"].'</td>
-            </tr>
-            <tr>
-                <td colspan="2">&nbsp;</td>
-            </tr>
-            <tr>
-                <td width="165" align="left" valign="top" style="border-bottom: 1px solid #282828;">&nbsp;</td>
-                <td width="435" align="left" valign="top" style="border-bottom: 1px solid #282828;">&nbsp;</td>
-            </tr>
-        </tbody>
+    </head>
+    <body>
+        <table border="0" cellpadding="0" cellspacing="0" width="600" align="center" style="">
+            <tbody>
+                <tr>
+                    <td width="210" bgcolor="" align="center" style="padding:20px 0;">
+                        <img src="http://amsala.com.mx/imgs/varias/amsala.jpg" style="display: inline-block;" width="155" height="85">
+                    </td>
+                </tr>
+            </tbody>
         </table>
         <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="font-family:Verdana, Geneva, sans-serif; color:#282828; font-size:12px;">
-            <tr>
-                <td width="140" align="left" valign="top">&nbsp;</td>
-            </tr>
-            <tr>
-                <td width="140" align="left" valign="top">&nbsp;</td>
-            </tr>
-            <tr>
-                <td height="27" align="center" valign="middle"><span class="nom"><strong style="color:#305496;font-size:18px;">Productos a cotizar:</strong></span></td>
-            </tr>
-            <tr>
-                <td width="140" align="left" valign="top">&nbsp;</td>
-            </tr>
-            <tr>
-                <td align="center" valign="middle">'.$cadena2.'</td>
-            </tr>
-            <tr>
-                <td align="left" valign="top" style="border-bottom: 1px solid #282828;">&nbsp;</td>
-            </tr>
-        </table>
-        <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="font-family:Verdana, Geneva, sans-serif; color:#666; font-size:12px;">
-            <tr>
-                <td height="43" align="center" style="color:#282828; font-size:11px;">&copy; '.date("Y").' AMSALA.</td>
-            </tr>
-        </table>
-</body>
-</html>';
+            <tbody>
+                <tr>
+                    <td width="165" align="left" valign="top" style="border-bottom: 1px solid #282828;">&nbsp;</td>
+                    <td width="435" align="left" valign="top" style="border-bottom: 1px solid #282828;">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td width="140" align="left" valign="top">&nbsp;</td>
+                    <td width="460" align="left" valign="top">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td height="27" align="left" valign="middle"><span class="nom"><strong>Nombre:</strong></span></td>
+                    <td height="27" align="left" valign="middle">'.$_POST["v1"].'</td>
+                </tr>
+                <tr>
+                    <td height="30" align="left" valign="middle"><strong>E-mail:</strong></td>
+                    <td align="left" valign="middle">'.$_POST["v2"].'</td>
+                </tr>
+                <tr>
+                    <td height="30" align="left" valign="middle"><strong>Ciudad:</strong></td>
+                    <td align="left" valign="middle">'.$_POST["v3"].'</td>
+                </tr>
+                <tr>
+                    <td height="30" align="left" valign="middle"><strong>Tel&eacute;fono:</strong></td>
+                    <td align="left" valign="middle">'.$_POST["v4"].'</td>
+                </tr>
+                <tr>
+                    <td height="30" align="left" valign="middle"><strong>Estado:</strong></td>
+                    <td align="left" valign="middle">'.$_POST["v5"].'</td>
+                </tr>
+                <tr>
+                    <td height="30" align="left" valign="middle"><strong>Tipo de Cliente:</strong></td>
+                    <td align="left" valign="middle">'.$_POST["v6"].'</td>
+                </tr>
+                <tr>
+                    <td height="27" align="left" valign="middle"><span class="nom"><strong>Preguntas y/o Comentarios:</strong></span></td>
+                    <td height="27" align="left" valign="middle">'.$_POST["v7"].'</td>
+                </tr>
+                <tr>
+                    <td colspan="2">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td width="165" align="left" valign="top" style="border-bottom: 1px solid #282828;">&nbsp;</td>
+                    <td width="435" align="left" valign="top" style="border-bottom: 1px solid #282828;">&nbsp;</td>
+                </tr>
+            </tbody>
+            </table>
+            <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="font-family:Verdana, Geneva, sans-serif; color:#282828; font-size:12px;">
+                <tr>
+                    <td width="140" align="left" valign="top">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td width="140" align="left" valign="top">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td height="27" align="center" valign="middle"><span class="nom"><strong style="color:#305496;font-size:18px;">Productos a cotizar:</strong></span></td>
+                </tr>
+                <tr>
+                    <td width="140" align="left" valign="top">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td align="center" valign="middle">'.$cadena2.'</td>
+                </tr>
+                <tr>
+                    <td align="left" valign="top" style="border-bottom: 1px solid #282828;">&nbsp;</td>
+                </tr>
+            </table>
+            <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="font-family:Verdana, Geneva, sans-serif; color:#666; font-size:12px;">
+                <tr>
+                    <td height="43" align="center" style="color:#282828; font-size:11px;">&copy; '.date("Y").' AMSALA.</td>
+                </tr>
+            </table>
+    </body>
+    </html>';
 
 
-// echo $cadena;
+    // echo $cadena;
 
-//----------------------------------------------------------------------------------
-//  ENVIO DE CORREO
-
-    // $mail = new PHPMailer();
-    // $mail->CharSet = 'UTF-8';
-    // $mail->From = $_POST["v2"];
-    // $mail->FromName = $_POST["v1"];
-    // $mail->Subject = "Pedidos y cotizaciones";
-    // $mail->Body = $cadena;
-    // $mail->IsHTML(true);
-    // $mail->AddAddress("ventas@amsala.com.mx");
-    // // $mail->AddAddress("web@dobleerre.com");
-    // $mail->Send();
-    // $mail->ClearAddresses();
-    // $mail->ClearAttachments();
+    //----------------------------------------------------------------------------------
+    //  ENVIO DE CORREO
 
 
-    require_once ("phpmailer/clsMail.php");
-    $mailSend = new clsMail();
-    $enviado =  $mailSend->metEnviar("ventas@amsala.com.mx",$_POST["v1"],$_POST['v2'],"Cotización desde la Web Amsala", $cadena);
+        require_once ("phpmailer/clsMail.php");
+        $mailSend = new clsMail();
+        $enviado =  $mailSend->metEnviar("ventas@amsala.com.mx",$_POST["v1"],$_POST['v2'],"Cotización desde la Web Amsala", $cadena);
 
 
 
+    //----------------------------------------------------------------------------------
+    //  REDIRECCIONAMIENTO
 
-//----------------------------------------------------------------------------------
-//  REDIRECCIONAMIENTO
+        setcookie( 'contact', "3004KSJDCOW123456", time()+120,"/" );
+        header('Location: gracias.html');
+        
+}else{
 
-    setcookie( 'contact', "3004KSJDCOW123456", time()+120,"/" );
-    header('Location: gracias.html');
-
-
+    header('Location: amsala.com.mx');
+        
+}
 
 
 
